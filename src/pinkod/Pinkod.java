76fd241f1efdae213909c44aca0cac46486cf027
Venc03 <vegi.dani1@gmail.com>
2023-09-13 13:50:19 +0200

    pinpad resz elkezdve

--- src/pinkod/Pinkod.java
+++ src/pinkod/Pinkod.java
@@ -1,21 +1,26 @@
 package pinkod;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Pinkod {
 
     static Scanner src = new Scanner(System.in);
+    static Random rnd = new Random();
+    static int min = 1;
+    static int max = 9;
+    static int rndlist[];
 
     public static void main(String[] args) {
-        Beker();
+        Pinpad();
     }
 
-    public static void Beker() {
-        System.out.println("Kérem adjon meg egy uj 4 szamu pint: ");
+    public static void Pinpad() {
+        System.out.println("Kérem adjon meg egy uj 4-6 szamu pint: ");
         int Pin = src.nextInt();
         int pinLength = Integer.toString(Pin).length();
         while (pinLength < 4 || pinLength > 6) {
-            System.out.println("-ERROR- Kérem 4 szamu legyen a pin: ");
+            System.out.println("-ERROR- Kérem 4-6 szamu legyen a pin: ");
             Pin = src.nextInt();
             pinLength = Integer.toString(Pin).length();
         }
@@ -27,17 +32,23 @@ public class Pinkod {
             int bekertPin = src.nextInt();
             if (bekertPin != Pin) {
                 System.out.println("Sikertelen belepes, kérem adja meg a kodot ujra(" + (i + 1) + "/3): ");
-            }else{
+            } else {
                 i = 3;
                 helyes = true;
             }
-            
 
         }
         if (helyes == true) {
             System.out.println("Sikeres belepes.");
         } else {
             System.out.println("Belepes megtagadva.");
+        }
+
+        //pinpad resz (meg folyamatban)
+        for (int i = 0; i < 8; i++) {
+            Integer.toString(rndlist[i]);
+            int random = rnd.nextInt(max - min + 1) + min;
+            random += rndlist[i];
         }
     }
 }
